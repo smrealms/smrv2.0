c218ecc32b0e4055a6ce23564ee4e8c6474d50d2
alexarias11007 <ada6@njit.edu>
2014-10-16 20:35:20 -0400

    Create login2.php

---
+++ htdocs/login2.php
@@ -0,0 +1,53 @@
+
+<body>
+<table cellspacing='0' cellpadding='0' border='0' width='500' height='100%'>
+<tr>
+	<td></td>
+	<td colspan='3' height='1' bgcolor='#0B8D35'></td>
+	<td></td>
+</tr>
+<tr>
+	<td width='1'>&nbsp;</td>
+	<td width='1' bgcolor='#0B8D35'></td>
+	<td align='left' valign='top' width='400' bgcolor='#06240E'>
+		<table width='100%' height='100%' border='0' cellspacing='3' cellpadding='3'>
+		<tr>
+			<td valign='top'>
+
+	<form action="login_processing2.php" method="post" style="margin-top:80px; margin-left:0px;">
+	<table width='100%' height='100%' border='0' cellspacing='3' cellpadding='3'>
+		<tr>
+			<td colspan='2'>&nbsp;</td>
+		</tr>
+		<tr>
+			<th colspan='2'>Login</th>
+		</tr>
+		<tr>
+			<td colspan='2'>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>User Name:</td>
+			<td><input type="text" class="rounded" size="30" maxlength="30" name="login"/></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td> <input type="password" class="rounded" size="30" maxlength="30" name="password"/><br></td>
+		</tr>
+			<td><input id="login_submit" name="login_submit" type="image" src="images/login_button.png"/></td>
+		</tr>
+	</form>
+	</table>
+	</td>
+		</tr>
+		</table>
+	</td>
+	<td width='1' bgcolor='#0B8D35'></td>
+	<td width='1'>&nbsp;</td>
+</tr>
+<tr>
+	<td></td>
+	<td colspan='3' height='1' bgcolor='#0b8d35'></td>
+	<td></td>
+</tr>
+</table>
+</body>
