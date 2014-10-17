de4d3f9399f4b4101b9bf0270dc8a668e488711b
alexarias11007 <ada6@njit.edu>
2014-10-16 20:34:48 -0400

    Create reset_password_processing2.php

---
+++ htdocs/reset_password_processing2.php
@@ -0,0 +1,56 @@
+<?php
+try {
+	require_once('config.inc');
+	require_once(ENGINE . 'Default/smr.inc');
+	require_once(get_file_loc('SmrAccount.class.inc'));
+	
+	
+	$password = $_REQUEST['password'];
+	if (strstr($password, '\'')) {
+		$msg = 'Illegal character in password detected! Don\'t use the apostrophe.';
+		header('Location: '.URL.'/error.php?msg=' . rawurlencode(htmlspecialchars($msg, ENT_QUOTES)));
+		exit;
+	}
+	
+	if (empty($password)) {
+		$msg = 'Password is missing!';
+		header('Location: '.URL.'/error.php?msg=' . rawurlencode(htmlspecialchars($msg, ENT_QUOTES)));
+		exit;
+	}
+	
+	$pass_verify = $_REQUEST['pass_verify'];
+	if ($password != $pass_verify) {
+		$msg = 'The passwords you entered do not match.';
+		header('Location: '.URL.'/error.php?msg=' . rawurlencode(htmlspecialchars($msg, ENT_QUOTES)));
+		exit;
+	}
+	
+	
+	// get this user from db
+	$login = $_REQUEST['login'];
+	if ($login == $password) {
+		$msg = 'Your chosen password is invalid!';
+		header('Location: '.URL.'/error.php?msg=' . rawurlencode(htmlspecialchars($msg, ENT_QUOTES)));
+		exit;
+	}
+	
+	// creates a new user account object
+	$account =& SmrAccount::getAccountByName($login);
+	$old_password = $_REQUEST['old_password'];
+	//$passwordReset = $_REQUEST['password_reset'];
+	if ($account==null || $account->checkPassword($old_password)) {
+		// unknown user
+		header('Location: '.URL.'/error.php?msg=' . rawurlencode('User does not exist or your current password is wrong.'));
+		exit;
+	}
+	
+	
+	$account->setPassword($password);
+	
+	
+	header('Location: '.URL.'/login.php');
+}
+catch(Exception $e) {
+	handleException($e);
+}
+?>
