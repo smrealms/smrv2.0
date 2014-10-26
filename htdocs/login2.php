c97458d009307b46b3cf73b8d783da17996c3925
alexarias11007 <ada6@njit.edu>
2014-10-26 15:12:02 -0400

    Update login2.php
    changed the css style colors

--- htdocs/login2.php
+++ htdocs/login2.php
@@ -1,39 +1,39 @@
 
-<body>
 <table cellspacing='0' cellpadding='0' border='0' width='500' height='100%'>
 <tr>
 	<td></td>
-	<td colspan='3' height='1' bgcolor='#0B8D35'></td>
+	<td colspan='3' height='1' style="background-color:#0F1541;"></td>
 	<td></td>
 </tr>
 <tr>
 	<td width='1'>&nbsp;</td>
-	<td width='1' bgcolor='#0B8D35'></td>
-	<td align='left' valign='top' width='400' bgcolor='#06240E'>
+	<td width='1' style="background-color:#0F1541;"></td>
+	<td align='left' valign='top' width='400' style="background-color:#475486;">
 		<table width='100%' height='100%' border='0' cellspacing='3' cellpadding='3'>
 		<tr>
 			<td valign='top'>
 
-	<form action="login_processing2.php" method="post" style="margin-top:80px; margin-left:0px;">
+	<form action="login_processing2.php" method="post" style="margin-top:8px; margin-left:0px;">
 	<table width='100%' height='100%' border='0' cellspacing='3' cellpadding='3'>
 		<tr>
 			<td colspan='2'>&nbsp;</td>
 		</tr>
 		<tr>
-			<th colspan='2'>Login</th>
+			<th colspan='2' style="background-color:#0F1541; color:white">Login</th>
 		</tr>
 		<tr>
 			<td colspan='2'>&nbsp;</td>
 		</tr>
 		<tr>
 			<td>User Name:</td>
-			<td><input type="text" class="rounded" size="30" maxlength="30" name="login"/></td>
+			<td><input type="text" class="rounded" size="30" maxlength="30" name="login" style="background-color:#0F1541;"/></td>
 		</tr>
 		<tr>
 			<td>Password:</td>
-			<td> <input type="password" class="rounded" size="30" maxlength="30" name="password"/><br></td>
+			<td> <input type="password" class="rounded" size="30" maxlength="30" name="password" style="background-color:#0F1541;"/><br></td>
 		</tr>
-			<td><input id="login_submit" name="login_submit" type="image" src="images/login_button.png"/></td>
+			<td><input type="button" onclick="return toggleVisibility('login')" value="Cancel" style="background-color:#0F1541; color:white;"/>
+			<input id="login_submit" name="login_submit" type="button" value="Submit" style="background-color:#0F1541; color:white;"/></td>
 		</tr>
 	</form>
 	</table>
@@ -41,13 +41,12 @@
 		</tr>
 		</table>
 	</td>
-	<td width='1' bgcolor='#0B8D35'></td>
+	<td width='1' style="background-color:#0F1541;"></td>
 	<td width='1'>&nbsp;</td>
 </tr>
 <tr>
 	<td></td>
-	<td colspan='3' height='1' bgcolor='#0b8d35'></td>
+	<td colspan='3' height='1' bstyle="background-color:#0F1541;"></td>
 	<td></td>
 </tr>
 </table>
-</body>
