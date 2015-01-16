cfb3972ac59f9bd04d8ed6e62ffbc5ac02ed132c
wcyrek <github@witoldcyrek.com>
2015-01-16 07:22:29 -0500

    Split combined view from old view
    Begin adding javix api for backend fetching of data

---
+++ htdocs/javix.php
@@ -0,0 +1,72 @@
+<?php
+try {
+	require_once('config.inc');
+	
+	
+	if (get_magic_quotes_gpc()) {
+	    function stripslashes_array($array)
+	    {
+	        return is_array($array) ? array_map('stripslashes_array', $array) : stripslashes($array);
+	    }
+	
+	    $_COOKIE = stripslashes_array($_COOKIE);
+	    $_FILES = stripslashes_array($_FILES);
+	    $_GET = stripslashes_array($_GET);
+	    $_POST = stripslashes_array($_POST);
+	    $_REQUEST = stripslashes_array($_REQUEST);
+	}
+	
+	header('Cache-Control: no-cache, must-revalidate');
+	//A date in the past
+	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+
+	// ********************************
+	// *
+	// * I n c l u d e s   h e r e
+	// *
+	// ********************************
+	
+	// We want these to be already defined as globals
+	$player=null;
+	$ship=null;
+	$sector=null;
+	$container=null;
+	$var=null;
+	$lock=false;
+	
+	// overwrite database class to use our db
+	require_once(LIB . 'Default/SmrMySqlDatabase.class.inc');
+	require_once(LIB . 'Default/Globals.class.inc');
+	
+	require_once(get_file_loc('smr.inc'));
+	
+	// new db object
+	$db = new SmrMySqlDatabase();
+	
+	// ********************************
+	// *
+	// * c h e c k   S e s s i o n
+	// *
+	// ********************************
+	
+	//echo '<pre>';echo_r($session);echo'</pre>';
+	//exit;
+	// do we have a session?
+	if (SmrSession::$account_id == 0) {
+		echo null;
+		exit;
+	}
+  
+	
+	if ($_REQUEST['action'] == 'get_local') {
+		$ThisPlayer =& SmrPlayer::getPlayer(SmrSession::$account_id, SmrSession::$game_id);
+		$ThisSector =& $ThisPlayer->getSector(); 
+		$ThisShip	=& $ThisPlayer->getShip();
+		require_once('../javix/get_local.inc');
+	}
+
+}
+catch(Exception $e) {
+	handleException($e);
+}
+?>
--- templates/Default/engine/Default/combined.php
+++ templates/Default/engine/Default/combined.php
@@ -4,55 +4,69 @@
 		$this->assign('FontSize', $FontSize-20);
 		$this->includeTemplate('includes/Head.inc'); ?>	
 	</head>
-	<body>
+	<body align="center">
 		<div id="menu_container">
 			<div id="menu">
-					<?php $this->includeTemplate('includes/LeftPanel.inc'); ?>
+					<?php $this->includeTemplate('combined/LeftPanel.inc'); ?>
 			</div>
 		</div>
-		<table class="m" align="center">
-			<tr>
-				<td class="l0 left_side">
-					<div class="left_side_item_wrapper"><?php
-						$this->includeTemplate('includes/SectorPlanet.inc');
-						$this->includeTemplate('includes/SectorPort.inc');
-						$this->includeTemplate('includes/SectorLocations.inc');
-						$this->includeTemplate('includes/SectorPlayers.inc',array('PlayersContainer'=>&$ThisSector));
-						$this->includeTemplate('includes/SectorForces.inc'); ?>
-					</div>
-				</td>
-				<td class="m0">
-					<div id="middle_panel"><?php
-						if(isset($PageTopic)) {
-							?><h1><?php echo $PageTopic; ?></h1><br><?php
-						}
-						if(isset($MenuItems)||isset($MenuBar)) { ?>
-							<div class="bar1">
-								<div><?php
-									if(isset($MenuItems)) {
-										$this->includeTemplate('includes/menu.inc');
-									}
-									else if(isset($MenuBar)) {
-										echo $MenuBar;
-									} ?>
+		<div>
+			<table align="center">
+				<tr>
+					<td>
+						<div style="text-align:justify">
+							<?php $this->includeTemplate('combined/TopPlayerInfo.inc'); ?>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<table align="center">
+						<tr>
+							<td style="vertical-align:top;">
+								<div class="left_side_item_wrapper"><?php
+									$this->includeTemplate('combined/SectorPlanet.inc');
+									$this->includeTemplate('combined/SectorPort.inc');
+									$this->includeTemplate('combined/SectorLocations.inc');
+									$this->includeTemplate('combined/SectorPlayers.inc',array('PlayersContainer'=>&$ThisSector));
+									$this->includeTemplate('combined/SectorForces.inc'); ?>
 								</div>
-							</div><br><?php
-						}
-						else if(isset($SubMenuBar)) {
-							echo $SubMenuBar;
-						}
-						$this->includeTemplate($TemplateBody); ?>
-					</div>
-				</td><?php
-				if (isset($GameID)) { ?>
-				<td class="r0">
-					<div id="right_panel">
-						<?php $this->includeTemplate('includes/RightPanel.inc'); ?>
-					</div>
-				</td><?php } ?>
-			</tr>
-		</table>
-		<?php $this->includeTemplate('includes/EndingJavascript.inc'); ?>
+							</td>
+							<td style="vertical-align:top;">
+								<div id="middle_panel"><?php
+									if(isset($PageTopic)) {
+										?><h1><?php echo $PageTopic; ?></h1><br><?php
+									}
+									if(isset($MenuItems)||isset($MenuBar)) { ?>
+										<div class="bar1">
+											<div><?php
+												if(isset($MenuItems)) {
+													$this->includeTemplate('combined/menu.inc');
+												}
+												else if(isset($MenuBar)) {
+													echo $MenuBar;
+												} ?>
+											</div>
+										</div><br><?php
+									}
+									else if(isset($SubMenuBar)) {
+										echo $SubMenuBar;
+									}
+									$this->includeTemplate($TemplateBody); ?>
+								</div>
+							</td>
+							<td style="vertical-align:top;">
+								<div id="right_panel">
+									COMBAT/EVENT LOG
+								</div>
+							</td>
+						</tr>
+					</table>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<?php $this->includeTemplate('combined/EndingJavascript.inc'); ?>
 	</body>
 	<img src="spinner.gif" id="spinner" style="width:30px; height:30px;position: absolute;top: 50%;left: 50%;"/>
 
