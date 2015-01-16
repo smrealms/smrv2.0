28aaaaeda374c235f2ad3816b225bfebb8972e0c
wcyrek <github@witoldcyrek.com>
2015-01-16 07:24:54 -0500

    Add old version to maintain beta as functional

---
+++ templates/Default/engine/Default/combined_old.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head><?php
+		$this->assign('FontSize', $FontSize-20);
+		$this->includeTemplate('includes/Head.inc'); ?>	
+	</head>
+	<body>
+		<div id="menu_container">
+			<div id="menu">
+					<?php $this->includeTemplate('includes/LeftPanel.inc'); ?>
+			</div>
+		</div>
+		<table class="m" align="center">
+			<tr>
+				<td class="l0 left_side">
+					<div class="left_side_item_wrapper"><?php
+						$this->includeTemplate('includes/SectorPlanet.inc');
+						$this->includeTemplate('includes/SectorPort.inc');
+						$this->includeTemplate('includes/SectorLocations.inc');
+						$this->includeTemplate('includes/SectorPlayers.inc',array('PlayersContainer'=>&$ThisSector));
+						$this->includeTemplate('includes/SectorForces.inc'); ?>
+					</div>
+				</td>
+				<td class="m0">
+					<div id="middle_panel"><?php
+						if(isset($PageTopic)) {
+							?><h1><?php echo $PageTopic; ?></h1><br><?php
+						}
+						if(isset($MenuItems)||isset($MenuBar)) { ?>
+							<div class="bar1">
+								<div><?php
+									if(isset($MenuItems)) {
+										$this->includeTemplate('includes/menu.inc');
+									}
+									else if(isset($MenuBar)) {
+										echo $MenuBar;
+									} ?>
+								</div>
+							</div><br><?php
+						}
+						else if(isset($SubMenuBar)) {
+							echo $SubMenuBar;
+						}
+						$this->includeTemplate($TemplateBody); ?>
+					</div>
+				</td><?php
+				if (isset($GameID)) { ?>
+				<td class="r0">
+					<div id="right_panel">
+						<?php $this->includeTemplate('includes/RightPanel.inc'); ?>
+					</div>
+				</td><?php } ?>
+			</tr>
+		</table>
+		<?php $this->includeTemplate('includes/EndingJavascript.inc'); ?>
+	</body>
+	<img src="spinner.gif" id="spinner" style="width:30px; height:30px;position: absolute;top: 50%;left: 50%;"/>
+
+	<script type="text/javascript">
+		window.onload=hideSpinner;
+ 
+		function hideSpinner() {
+			document.getElementById('spinner').style.display = "none";
+		}
+	</script>
+</html>
--- templates/Default/engine/Default/skeleton.php
+++ templates/Default/engine/Default/skeleton.php
@@ -1,7 +1,7 @@
 <?php
 	//Branch the old views vs the new view
 	if ($TemplateBody == 'map_combined.php') { 
-		require('combined.php');
+		require('combined_old.php');
 	} else {
 		require('regular.php');
 	}
